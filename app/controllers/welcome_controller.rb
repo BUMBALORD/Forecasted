dc28ba842e722b3aff5aee5aa3cd75f97ab2c41d
BUMBALORD <davidhallinan@gmail.com>
2016-06-28 15:10:51 -0700

    remove old code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,5 @@
 class WelcomeController < ApplicationController
-  # require 'forecast_io'
+
   def index
-  #   ForecastIO.api_key = ENV["forecast_api_key"]
-  #   location = Geocoder.search('3033 Dana St,Berkeley,Ca,94705')
-  #   latitude = location[0].latitude
-  #   longitude = location[0].longitude
-  #   response = ForecastIO.forecast(latitude,longitude)
-  #   currently = response.currently
-  #   @summary = currently.summary
-  #   @temp = currently.temperature
   end
 end
